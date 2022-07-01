ddd2145e13359bb2697932545207f6edbe6bccd7
Amy Woodworth <woodworthcommunications@gmail.com>
2022-06-30 21:55:26 -0500

    Adjust location.html. Update app.js.

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 const map = L.map('leaflet-map', {
   center: [41.481310, -71.310410],
   zoom: 16,
-  scrollWheelZoom: false
+  scrollWheelZoom: false,
+  zoomControl: false
 });
 
-const greenIcon = L.icon({
+const mapIcon = L.icon({
   iconUrl: 'assets/icon-location.svg',
   iconSize: [66, 88]
 });
@@ -17,15 +18,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 L.marker(
   [41.481310, -71.310410], { 
-    icon: greenIcon,
+    icon: mapIcon,
     alt: 'Modern Art Gallery' 
 })
 .addTo(map)
 .bindPopup('Modern Art Gallery');
-
-// To do: if map doesn't display, show fallback-map div with background images
-//
-// When location.html loads, check for leaflet.js map
-// if leaflet.js map fails
-// remove class hidden from fallback-map
-// add class hidden to leaflet-map
